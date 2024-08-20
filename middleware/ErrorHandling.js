aa632d12cf3b5b82396c022709f31231780ad6f4
AChar1f <acharif41@gmail.com>
2024-08-20 14:33:14 +0200

    endpoints table added and adjustments

--- middleware/ErrorHandling.js
+++ middleware/ErrorHandling.js
@@ -2,8 +2,7 @@ function errorHandling(err, req, res, next) {
     if (err || res.statusCode >= 400) {
         res.json(
             {
-                staus: err.staus ||
-                    res.statusCode || 500,
+                status: err.status || res.statusCode || 500,
                 err: "An error occurred =(. Please try again later."
             }
     )
